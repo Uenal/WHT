d11cb0976e5a6c1dbb5535fa377c9267d68660f6
Muhammet Ali Bekiroglu <muhammetalibekiroglu@maleeb.localdomain>
2016-06-27 15:13:00 +0200

    + Add warning message field to login Frame + Add warning messages for (Username empty, User not found, Password is false) + Add System.exit(1) to kill the process with the cancel button

--- src/de/bht/wht/uebung/Login.java
+++ src/de/bht/wht/uebung/Login.java
@@ -1,5 +1,7 @@
 package de.bht.wht.uebung;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
@@ -10,8 +12,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
-import java.util.Map;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -22,12 +22,14 @@ import javax.swing.JTextField;
 public class Login extends JDialog {
  
 	HashMap<String, String> personen = new HashMap<String, String>();
-	JLabel nameLabel = new JLabel("Name : ");
-	JLabel passwordLabel = new JLabel("Password : ");
-    String user = new String();
+	
+	JLabel warningLabel = new JLabel("Bitte geben Sie Benutzername und Kennwort ein");
+	
+	JLabel nameLabel = new JLabel("Name: ");
 	JTextField nameField = new JTextField();
+	JLabel passwordLabel = new JLabel("Password: ");
 	JPasswordField passwordField = new JPasswordField();
- 
+	
 	JButton okButton = new JButton("OK");
 	JButton cancelButton = new JButton("Cancel");
  
@@ -38,7 +40,7 @@ public class Login extends JDialog {
 	}
  
 	private void setupUser() {
-		// TODO Auto-generated method stub	
+		
 		personen.put("User1", "x401");
 		personen.put("User2", "x4aq");
 		personen.put("User3", "x4dc");
@@ -65,11 +67,13 @@ public class Login extends JDialog {
 		personen.put("User22", "iref");
 		personen.put("User23", "ylkh");
 		personen.put("User24", "231o");
+		
 	}
 
 	public void setupUI() {
  
 		this.setTitle("Login");
+		warningLabel.setForeground(Color.green);
  
 		JPanel topPanel = new JPanel(new GridBagLayout());
 		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -103,11 +107,17 @@ public class Login extends JDialog {
 		gbc.gridy = 1;
 		gbc.weightx = 1;
 		topPanel.add(passwordField, gbc);
+		
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.weightx = 1;
+		topPanel.add(warningLabel, gbc);
  
 		this.add(topPanel);
  
 		this.add(buttonPanel, BorderLayout.SOUTH);
- 
+		
 	}
  
 	private void setUpListeners() {
@@ -126,35 +136,68 @@ public class Login extends JDialog {
  
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				login();
+				
+				if(!nameField.getText().isEmpty()) {
+					
+					if(personen.get(nameField.getText()) != null) {
+					
+						if(personen.get(nameField.getText()).equals(passwordField.getText())) {
+							
+							login();
+							
+						} else {
+							
+							warningLabel.setForeground(Color.red);
+							warningLabel.setText("Das Kennwort ist falsch.");
+							
+						}
+					
+					} else {
+						
+						warningLabel.setForeground(Color.red);
+						warningLabel.setText("Der Benutzernamen wurde nicht gefunden.");
+						
+					} 
+					
+				} else {
+					
+					warningLabel.setForeground(Color.red);
+					warningLabel.setText("Bitte geben Sie den Benutzernamen ein.");
+					
+				}
+				
 			}
+			
 		});
  
 		cancelButton.addActionListener(new ActionListener() {
  
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Login.this.setVisible(false);
+				System.exit(1);
 			}
 		});
 	}
 	
 	public void login() {
-		if(personen.get(nameField.getText()) != null && personen.get(nameField.getText()).equals(passwordField.getText())){
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						
-						
-						ExperimentGUI window = new ExperimentGUI(nameField.getText());
-						window.frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		
+		EventQueue.invokeLater(new Runnable() {
+			
+			public void run() {
+				try {
+					
+					ExperimentGUI window = new ExperimentGUI(nameField.getText());
+					window.frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		}
+			}
+			
+		});
+		
 		Login.this.setVisible(false);
+		
 	}
 	
 	/**
